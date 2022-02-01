c2e79d5335c3fc2663c467b745df9e90ca1b60b4
Alexander Holden <91601068+al3xholden@users.noreply.github.com>
2022-02-01 01:48:43 +0000

    01/02/2022

--- Main.cpp
+++ Main.cpp
@@ -4,6 +4,8 @@
 #include <vector>
 #include <functional>
 #include <fstream>
+#include <mutex>
+//#import md5.h
 
 using namespace std;
 
@@ -87,12 +89,29 @@ private:
 
 class person
 {
+    int cardID;
+    string name = "";
+    string CardType = "";
+    string emergency = "";
 
+public:
+    string getName() { return name; } // Gets name of Person
+    void setName(string newName) { name = newName; } //Set name of a person
+
+
+private:
+
+};
+
+class Log 
+{
+    ifstream swipeLog ("swipeLog.txt");
 };
 
 struct student : public person // Structure for student role, extends from person class.
 {
 
+    person.CardID();
 };
 
 struct staff_member : public person // Structure for staff member role, extends from person class.
@@ -150,16 +169,66 @@ struct secure_room : public room // Structure for secure_room, extends from base
 
 };
 
+// Login / System Security - Add MD5 encryption here
+int login() {
+    string pass = "";
+    char ch;
+    cout << "\n\n\n\n\n\n\n\n\t\t\t     ACCESS CONTROL SYSTEM \n\n";
+    cout << "\t\t\t------------------------------";
+    cout << "\n\t\t\t\t     LOGIN \n";
+    cout << "\t\t\t------------------------------\n\n";
+    cout << "\t\t\tEnter Password: ";
+    ch = _getch();
 
+    while (ch != 13) {
+        pass.push_back(ch);
+        cout << '*';
+        ch = _getch();
+    }
+    if (pass == "admin") {
+        cout << "\n\n\n\t\t\tAccess Granted! \n";
+        system("CLS");
+    }
+    else {
+        cout << "\n\n\t\t\tAccess denied\n\t\t\tPlease Try Again\n\n";
+        system("PAUSE");
+        system("CLS");
+        login();
+    }
+
+    return 0;
+};
+
+//Initiate login before Main so that I can return to main without having to re-login.
+//----------------------------------------- login_BeforeMain is fully functional. Deactivated for now for development / testing purposes.
+class login_BeforeMain {
+
+public:
+    // Constructor of the class
+    login_BeforeMain()
+    {
+
+        // Call the other function
+        func();
+    }
+
+    // Function to get executed before main()
+    void func()
+    {
+       // int login(); //----------------------------------------- Fully functional. Deactivated for now for development / testing purposes.
+       // login();     //----------------------------------------- Fully functional. Deactivated for now for development / testing purposes.
+    }
+}; 
+
+// Global variable to declare
+// the object of class login_BeforeMain
+login_BeforeMain obj;
+
+
+// What happens after login? = Below
 
 int main()
 {
-
-    // Instantiates and Initiates login 
-    int login();
-    login();
-
-    // What happens after login? = Below
 
     // MENU //
     int inputchoice = 0; // Menu choice - Default = 0
@@ -219,52 +288,63 @@ int main()
     }
 
     // Error handling - If the user inputs a a value that's not 1-7.
+    /*
     else  
     {
         cout << " Error! Please choose an existing option Press any key to continue.";
-        _getch();
+        cout << "\n";
+        system("CLS");
         return main();
     }
+    */
 
 };
-
-// Login / System Security - Add MD5 encryption here
-int login() {
-    string pass = "";
-    char ch;
-    cout << "\n\n\n\n\n\n\n\n\t\t\t     ACCESS CONTROL SYSTEM \n\n";
-    cout << "\t\t\t------------------------------";
-    cout << "\n\t\t\t\t     LOGIN \n";
-    cout << "\t\t\t------------------------------\n\n";
-    cout << "\t\t\tEnter Password: ";
-    ch = _getch();
-
-    while (ch != 13) {
-        pass.push_back(ch);
-        cout << '*';
-        ch = _getch();
-    }
-    if (pass == "admin") {
-        cout << "\n\n\n\t\t\tAccess Granted! \n";
-        system("CLS");
-    }
-    else {
-        cout << "\n\n\t\t\tAccess denied\n\t\t\tPlease Try Again\n\n";
-        system("PAUSE");
-        system("CLS");
-        login();
-    }
-
-    return 0;
-};
-
 
 // 1 view and manage rooms
 int choice1() // view and manage rooms
 {
     system("CLS");
-    cout << " | ID | Name          | Card Type     | Emergency   " << endl;
-    cout << " =====+===============+===============+=============\n";
+    cout << " | ID   | Name          | Card Type     | Emergency   " << endl;
+    cout << " =======+===============+===============+=============\n";
+
+    int inputchoice = 0; // Menu choice - Default = 0
+    cout << endl;
+    cout << " 1 Add a room. \n";
+    cout << " 2 Edit or delete a room. \n";
+    cout << " 3 Save room data. \n";
+    cout << " 4 Reload rooms file.\n";
+    cout << endl;
+    cout << " Choice : ";
+    cin >> inputchoice;
+    cout << endl;
+
+    //1 Add a room
+    if(inputchoice==1) 
+    {
+        cout << "hellooo";
+    
+    }
+
+    // 2 Edit or delete a room
+    if (inputchoice == 2)
+    {
+        cout << "hellooo";
+
+    }
+
+    // 3 Save room data
+    if (inputchoice == 3)
+    {
+        cout << "hellooo";
+
+    }
+
+    // 4 Reload rooms file
+    if (inputchoice == 4)
+    {
+        cout << "hellooo";
+
+    }
 
     return 0;
 }
@@ -272,49 +352,30 @@ int choice1() // view and manage rooms
 // 2 view and manage ID cards
 int choice2() // view and manage rooms
 {
-    system("CLS");
-    cout << " | ID | Name          | Card Type     | Emergency   " << endl;
-    cout << " =====+===============+===============+=============\n";
-
     return 0;
 }
 
 // 3 simulate a card swipe
 int choice3() // view and manage rooms
 {
-    system("CLS");
-    cout << " | ID | Name          | Card Type     | Emergency   " << endl;
-    cout << " =====+===============+===============+=============\n";
-
     return 0;
 }
 
 // 4 put a room in or out of emergency state
 int choice4() // view and manage rooms
 {
-    system("CLS");
-    cout << " | ID | Name          | Card Type     | Emergency   " << endl;
-    cout << " =====+===============+===============+=============\n";
-
     return 0;
 }
 
 // 5 view swipe log
 int choice5() // view and manage rooms
 {
-    system("CLS");
-    cout << " | ID | Name          | Card Type     | Emergency   " << endl;
-    cout << " =====+===============+===============+=============\n";
-
     return 0;
 }
 
 // 6 manage preferences
 int choice6() // view and manage rooms
 {
-    system("CLS");
-    cout << " | ID | Name          | Card Type     | Emergency   " << endl;
-    cout << " =====+===============+===============+=============\n";
-
     return 0;
 }
+
